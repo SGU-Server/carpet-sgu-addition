ae43d15036b21d398e8a9aa011cdad2483c59cb6
eddie1023-wq <eddie1023@apps.ntpc.edu.tw>
2026-07-27 16:22:07 +0800

    feat(rule): add spectator portal teleport

--- src/main/java/org/carpet/sgu/SguSettings.java
+++ src/main/java/org/carpet/sgu/SguSettings.java
@@ -17,4 +17,9 @@ public class SguSettings {
             categories = {FEATURE, EXPERIMENTAL,SGU}
     )
     public static boolean reverseBlockPosTraversal = false;
+
+    @Rule(
+            categories = {FEATURE, EXPERIMENTAL, SGU}
+    )
+    public static boolean spectatorPortalTeleport = false;
 }
